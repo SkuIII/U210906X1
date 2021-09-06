8670b9bc6a3bda32032b0fff1052793a008eff41
Oscar <oscargus0247@gmail.com>
2021-09-06 14:22:04 +0200

    Färdiggjorde del som skriver ut antal ord

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -7,6 +7,19 @@ namespace Exercise1
         static void Main(string[] args)
         {
             Console.WriteLine("Skriv en mening:");
+            string sentence = Console.ReadLine(); 
+            int wordCount = 0;
+
+            for (int i = 0; i < sentence.Length - 1; i++)
+            {
+                if (sentence[i] == ' ' && Char.IsLetter(sentence[i + 1]) && (i > 0))
+                {
+                    wordCount++;
+                }
+            }
+            wordCount++;
+
+            Console.WriteLine("Antalet ord i meningen är: " + wordCount);
         }
     }
 }
