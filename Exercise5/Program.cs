324aa725ba765418c88da60cfe040344dadefa7d
Oscar <oscargus0247@gmail.com>
2021-09-07 17:46:55 +0200

    Kommenterade koden

--- Exercise5/Program.cs
+++ Exercise5/Program.cs
@@ -4,10 +4,6 @@ namespace Exercise5
 {
     class Program
     {
-        //Låt användaren mata in ett ord i taget
-        //Avsluta programmet om användaren matar in AVSLUTA
-        //Skriv ut din mening just nu är: skriv ut alla inmatade ord
-
         static void Main(string[] args)
         {
             Console.WriteLine("Välkommen! Här kan du bygga en mening med ett ord i taget, skriv avsluta för att avsluta!");
@@ -18,11 +14,13 @@ namespace Exercise5
                 Console.WriteLine("Mata in ett ord:");
                 string input = Console.ReadLine();
 
+                // Om input är avsluta (ToLower för att undvika case sensitive), avsluta programmet
                 if (input.ToLower() == "avsluta")
                 {
                     break;
                 }
 
+                // Lägg till ordet i meningen
                 sentence += $"{input} ";
             }
 
