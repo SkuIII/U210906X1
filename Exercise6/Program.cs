2959f67fcd7d6834026df8c7a7c3b2aacfa91d9d
Oscar <oscargus0247@gmail.com>
2021-09-07 19:47:34 +0200

    Färdiggjorde uppgift

--- Exercise6/Program.cs
+++ Exercise6/Program.cs
@@ -6,7 +6,24 @@ namespace Exercise6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Datorn genererade ett tal mellan 1-21, gissa vilket!");
+            // Generera ett random tal
+            Random rand = new Random();
+            int number = rand.Next(1, 21);
+
+            while (true)
+            {
+                int answer = Convert.ToInt32(Console.ReadLine()); // input
+
+                if (answer == number)
+                {
+                    Console.WriteLine($"Grattis! Du gissade rätt, talet var {number}");
+                    break;
+                }
+
+                Console.WriteLine("Du gissade fel, gissa igen!");
+            }
+
         }
     }
 }
